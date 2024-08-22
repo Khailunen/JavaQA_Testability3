6cfd357a076001fef4e84fcae80b83d8684b241f
Karina Khaltunen <khaltunen@bk.ru>
2024-08-22 22:00:18 +0300

    modified code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,7 @@
 public class CreditPaymentService {
     public double calculate(int credit, double annualRatePercent, int timeMonth) {
         double variable = (annualRatePercent / 12) / 100;
-        double coefficient = (variable * Math.pow((variable + 1), timeMonth)) /
-                (Math.pow((variable + 1), timeMonth) - 1);
+        double coefficient = (variable * Math.pow((variable + 1), timeMonth)) / (Math.pow((variable + 1), timeMonth) - 1);
         double paymentАmount = credit * coefficient;
         return (int) paymentАmount;
     }
